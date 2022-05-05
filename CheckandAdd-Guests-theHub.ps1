77ba5db700ff26438aeb275698aaef733a8e46a7
Kristopher Roy <kristopher.roy@gmail.com>
2022-05-05 14:22:03 -0700

    removed a loop and fixed an if else

--- CheckandAdd-Guests-theHub.ps1
+++ CheckandAdd-Guests-theHub.ps1
@@ -1,9 +1,9 @@
-$ver = '2.04'
+$ver = '2.05'
 <#
 Created By: Kristopher Roy
 Last Updated By: BTL
 Created On: 29Nov2021
-Last Updated On: 27APR2022
+Last Updated On: 05May2022
 #>
 
 #Begin Script
@@ -87,32 +87,54 @@ Connect-AzureAD -Credential $cred
 $users = (InputBox -header "Email List" -text "Please List Emails from ticket to be added" -icon "C:\Windows\SystemApps\Microsoft.Windows.SecHealthUI_cw5n1h2txyewy\Assets\Account.theme-light.ico").split('')
 
 #Loop through and check users then add them as Guest accounts if missing, and then add them to theHub Guest users group
-FOREACH($User in ($users.split('')))
+#Loop through and check users then add them as Guest accounts if missing, and then add them to theHub Guest users group
+FOREACH($User in ($users.split('')|where{$_}))
 {
+    $user
     $aaduser = Get-AzureADUser -filter "DisplayName eq '$user'"|select *
     IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "Mail eq '$user'"|select *}
     IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "UserPrincipalName eq '$user'"|select *}
+    If($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "OtherMails eq '$user'"|select *}
     If($aaduser -eq $null)
     {
         write-host "No User Account $user"
         $DisplayName = (($user.split("@")[0].split(".")[0]).substring(0,1).toupper()+($user.split("@")[0].split(".")[0]).substring(1).toLower())+" "+(($user.split("@")[0].split(".")[1]).substring(0,1).toupper()+($user.split("@")[0].split(".")[1]).substring(1).toLower())
         powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('Creating Guest account for $Displayname')}"
         New-AzureADMSInvitation -InvitedUserDisplayName $DisplayName -InvitedUserEmailAddress $user -InviteRedirectURL https://gtinetorg.sharepoint.com/sites/theHUB -SendInvitationMessage $true
-        start-sleep 10
+        start-sleep 15
         $aaduser = Get-AzureADUser -filter "DisplayName eq '$user'"
         IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "Mail eq '$user'"}
-		powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($aaduser.userprincipalname)" exists adding to groups')}"
+        If($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "OtherMails eq '$user'"|select *}
+        IF($aaduser -eq $null)
+        {
+            powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($user)" not found, waiting 30 seconds then trying again')}"
+            start-sleep 30
+            $aaduser = Get-AzureADUser -filter "DisplayName eq '$user'"
+            IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "Mail eq '$user'"}
+            IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "UserPrincipalName eq '$user'"|select *}
+            If($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "OtherMails eq '$user'"|select *}
+            IF($aaduser -eq $null)
+            {
+                powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($user)" still not found, waiting another 30 seconds then trying again')}"
+                start-sleep 30
+                $aaduser = Get-AzureADUser -filter "DisplayName eq '$user'"
+                IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "Mail eq '$user'"}
+                IF($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "UserPrincipalName eq '$user'"|select *}
+                If($aaduser -eq $null){$aaduser = Get-AzureADUser -filter "OtherMails eq '$user'"|select *}
+                IF($aaduser -eq $null){powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($user)" still not found, please end and troubleshoot, user not added to group')}"}
+                exit
+            }
+        }
+        powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($aaduser.userprincipalname)" exists adding to groups')}"
         Add-AzureADGroupMember -ObjectId 1bc2445f-5478-4d22-aac1-29c000817f7b -RefObjectID $aaduser.ObjectId
     }
     ELSE
     {
+        write-host $aaduser.UserPrincipalName
         powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('"($aaduser.userprincipalname)" exists adding to groups')}"
-		Add-AzureADGroupMember -ObjectId 1bc2445f-5478-4d22-aac1-29c000817f7b -RefObjectID $aaduser.ObjectId
+        Add-AzureADGroupMember -ObjectId 1bc2445f-5478-4d22-aac1-29c000817f7b -RefObjectID $aaduser.ObjectId
     }
-    #$User.UserType = $aaduser.UserType
-    #$User.UPN = $aaduser.UserPrincipalName
-    #$User.DisplayName = $aaduser.DisplayName
-    #$User.ObjectID = $aaduser.ObjectID
 }
 
 #$USERS|export-csv C:\projects\gtil\TheHub-Import-11Feb22-GrabbedandAddedUsers.csv -NoTypeInformation
+
